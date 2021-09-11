ca0da4bea0b59f223ba22a1d1e53ebdd31a9f048
cstef <colin@petit-suisse.fr>
2021-09-11 19:51:46 +0200

    Basically the whole extension

---
+++ src/extension.ts
@@ -0,0 +1,78 @@
+import { exec } from "child_process";
+import * as vscode from "vscode";
+
+const run = (command: string, output: vscode.OutputChannel) =>
+    new Promise<string>((resolve, reject) => {
+        exec(command, (err, stdout, stderr) => {
+            if (err) {
+                output.appendLine(`${err}`);
+                output.appendLine(stderr);
+                reject(err.message);
+            } else {
+                resolve(stdout);
+                output.appendLine(stdout);
+            }
+        });
+    });
+
+const format = async (input: string, output: vscode.OutputChannel) => {
+    const command = `cat <<EOF |ktlint --stdin -F\n${input}\nEOF`;
+    return await run(command, output);
+};
+
+export const activate = (context: vscode.ExtensionContext) => {
+    const output = vscode.window.createOutputChannel("Kotlin Formatter");
+    output.appendLine("Enabled Kotlin-Formatter");
+    let command = vscode.commands.registerCommand("kotlin-formatter.formatKotlin", async () => {
+        const { activeTextEditor } = vscode.window;
+
+        if (
+            activeTextEditor &&
+            ["kotlin", "kotlinscript"].includes(activeTextEditor.document.languageId)
+        ) {
+            const { document } = activeTextEditor;
+            const newFile = await format(document.getText(), output);
+            const edit = new vscode.WorkspaceEdit();
+            edit.replace(
+                document.uri,
+                new vscode.Range(
+                    new vscode.Position(0, 0),
+                    new vscode.Position(
+                        document.lineCount - 1,
+                        document.lineAt(document.lineCount - 1).range.end.character
+                    )
+                ),
+                newFile
+            );
+
+            return vscode.workspace.applyEdit(edit);
+        }
+    });
+
+    let formatter = vscode.languages.registerDocumentFormattingEditProvider(
+        [{ language: "kotlin" }, { language: "kotlinscript" }],
+        {
+            provideDocumentFormattingEdits: async (document) => {
+                try {
+                    const newFile = await format(document.getText(), output);
+                    const edit = vscode.TextEdit.replace(
+                        new vscode.Range(
+                            new vscode.Position(0, 0),
+                            new vscode.Position(
+                                document.lineCount - 1,
+                                document.lineAt(document.lineCount - 1).range.end.character
+                            )
+                        ),
+                        newFile
+                    );
+                    return [edit];
+                } catch (e) {
+                    output.appendLine(e as string);
+                    vscode.window.showErrorMessage(`An Error occured when formatting: ${e}`);
+                    return [];
+                }
+            },
+        }
+    );
+    context.subscriptions.push(command, formatter);
+};
